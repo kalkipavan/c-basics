5d00cd3b56a72016fd8473faee08b18704aa2875
kalkipavan <92910171+kalkipavan@users.noreply.github.com>
2021-10-23 12:21:20 +0530

    Add files via upload

---
+++ hello world name 2.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+void main() 
+{
+	   printf("roll no-20p35a0119\n\nname-kalki pavan\n\nbranch-civil\n\ncollege-acet\n\n");
+}
---
+++ student age.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+void main() 
+{
+        printf("student\t age\t cgpa\n");
+        printf("s1\t 19\t 8.5\n");
+        printf("s2\t 18\t 9.0\n");
+}

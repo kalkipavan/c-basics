9038e41b45c4e5f0c5369847ee26f791a5da7df7
kalkipavan <92910171+kalkipavan@users.noreply.github.com>
2021-11-11 17:25:42 +0530

    Add files via upload

---
+++ rectangle.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+	int l,w,a,p;
+	printf("enter l,w");
+	scanf("%d%d",&l,&w);
+	a=l*w;
+	p=2*(l+w);
+	printf("area pf the rectangle %d\n",a);
+	printf("perimeter of the rectangle %d",p);
+}
